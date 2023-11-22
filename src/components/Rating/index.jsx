62ed390d4d564d0b2bf80e1048e01f61afb1e824
pedrodecf <contato.pedrodecf@gmail.com>
2023-11-22 16:12:55 -0300

    adicionar hook de autenticação

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -2,6 +2,7 @@ import { Container, FullStar, EmptyStar } from "./styles"
 
 export function Rating({ $star }) {
   return (
-    <Container $star={$star}>{$star ? <FullStar /> : <EmptyStar />}</Container>
+    <Container $star={$star}>
+      {$star ? <FullStar /> : <EmptyStar />}</Container>
   )
 }
